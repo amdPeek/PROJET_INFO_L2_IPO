f8b3e7906327dafc4bdbdba2ee8415811dd86c68
theo <theomanea9@gmail.com>
2020-11-10 19:07:23 +0100

    structure

---
+++ src/com/company/Element.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.awt.*;
+
+
+public class Element extends Case {
+    public final Color color;
+
+    public Element(int absc, int ord, Color color) {
+        super(absc, ord);
+        this.color = color;
+    }
+    
+    public Element(Case c, Color color) {
+        super(c.absc, c.ord);
+        this.color = color;
+    }
+    
+}
